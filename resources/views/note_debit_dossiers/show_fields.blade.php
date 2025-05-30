c17c74ee2dc7c7bae3d357db0aaccd92aa672c99
Ayoub Raja <a.beraouz@opti,gov.com>
2025-05-30 21:45:04 +0100

    push

--- resources/views/note_debit_dossiers/show_fields.blade.php
+++ resources/views/note_debit_dossiers/show_fields.blade.php
@@ -147,104 +147,87 @@
 </div>
 
 @if (isset($noteDebitDossier->id))
-    <!-- Numfacturation Field -->
     <div class="form-group col-md-3 col-lg-3">
         {!! Form::label('numnotedebit', 'N° Note de Débit :') !!}
         <p>{{ $noteDebitDossier->numnotedebit }}</p>
     </div>
 @endif
 
-<!-- Datefacturation Field -->
 <div class="form-group col-md-3 col-lg-3">
     {!! Form::label('datenotationdebit', 'Date de Note de Débit :') !!}
     <p>{{ $noteDebitDossier->datenotationdebit }}</p>
 </div>
 
-<!-- Mod Paiement Field -->
 <div class="form-group col-md-3 col-lg-3">
     {!! Form::label('mod_paiement', 'Mode de paiement :') !!}
     <p>{{ $noteDebitDossier->mod_paiement }}</p>
 </div>
 
-<!-- Montantdebit Field -->
 <div class="form-group col-md-3 col-lg-3">
     {!! Form::label('montantdebit', 'Montant a payer pour le Débit ( en EURO ) :') !!}
     <p>{{ $noteDebitDossier->montantdebit }}</p>
 </div>
 
-<!-- Designation1 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('designation1', 'Désignation 1 :') !!}
     <p>{{ $noteDebitDossier->designation1 }}</p>
 </div>
 
-<!-- Note Debit 1 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('notedebit1', 'A Noter de Débit 1 :') !!}
     <p>{{ $noteDebitDossier->notedebit1 }}</p>
 </div>
 
-<!-- Designation2 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('designation2', 'Désignation 2 :') !!}
     <p>{{ $noteDebitDossier->designation2 }}</p>
 </div>
 
-<!-- Note Debit 2 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('notedebit2', 'A Noter de Débit 2 :') !!}
     <p>{{ $noteDebitDossier->notedebit2 }}</p>
 </div>
 
-<!-- Designation3 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('designation3', 'Désignation 3 :') !!}
     <p>{{ $noteDebitDossier->designation3 }}</p>
 </div>
 
-<!-- Note Debit 3 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('notedebit3', 'A Noter de Débit 3 :') !!}
     <p>{{ $noteDebitDossier->notedebit3 }}</p>
 </div>
 
-<!-- Designation4 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('designation4', 'Désignation 4 :') !!}
     <p>{{ $noteDebitDossier->designation4 }}</p>
 </div>
 
-<!-- Note Debit 4 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('notedebit4', 'A Noter de Débit 4 :') !!}
     <p>{{ $noteDebitDossier->notedebit4 }}</p>
 </div>
 
-<!-- Designation5 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('designation5', 'Désignation 5 :') !!}
     <p>{{ $noteDebitDossier->designation5 }}</p>
 </div>
 
-<!-- Note Debit 5 Field -->
 <div class="form-group col-md-6 col-lg-6">
     {!! Form::label('notedebit5', 'A Noter de Débit 5 :') !!}
     <p>{{ $noteDebitDossier->notedebit5 }}</p>
 </div>
 
-<!-- A Paye Field -->
 <div class="form-group col-md-4 col-lg-4">
     {!! Form::label('a_paye', 'Devise sur la Note de débit :') !!}
     <p>{{ $noteDebitDossier->a_paye }}</p>
 </div>
 
-<!-- Description Field -->
 <div class="form-group col-md-4 col-lg-4">
     {!! Form::label('description', __('models/noteDebitDossiers.fields.description') . ' :') !!}
     <p>{{ $noteDebitDossier->description }}</p>
 </div>
 
-<!-- A Estimer Field -->
 <div class="form-group col-md-4 col-lg-4">
     {!! Form::label('a_estimer', 'Equivalent en Dirhams	 :') !!}
     <p>{{ $noteDebitDossier->a_estimer }}</p>
